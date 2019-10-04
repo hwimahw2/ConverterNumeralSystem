e3a3f1fd8b1f1995c1fa280462e72ac1d5bdc2bd
Mansur <hwimahw@gmail.com>
2019-10-04 16:34:39 +0300

    Modify and add exception pages

--- src/main/java/ru/ncd/Servlet.java
+++ src/main/java/ru/ncd/Servlet.java
@@ -10,12 +10,10 @@ import java.io.PrintWriter;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        PrintWriter pw = response.getWriter();
-        String number = request.getParameter("number");
-        int a = Integer.parseInt(request.getParameter("a"));
-        int b = Integer.parseInt(request.getParameter("b"));
-        System.out.println(a + " " + b + " " + number);
         try {
+            String number = request.getParameter("number");
+            int a = Integer.parseInt(request.getParameter("a"));
+            int b = Integer.parseInt(request.getParameter("b"));
             String result = Converter.countResult(number, a, b);
           //  ServletConfig servletConfig = getServletConfig();
           //  ServletContext servletContext = servletConfig.getServletContext();
@@ -26,9 +24,15 @@ public class Servlet extends javax.servlet.http.HttpServlet {
             servletContext.setAttribute("result", result);
             RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/mainPage.jsp");
             requestDispatcher.forward(request, response);
-        }catch(Exception e){
-            System.out.println(121212);
-            return;
+        }catch(OverFlowException e){
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/overFlowExceptionPage.jsp");
+            requestDispatcher.forward(request, response);
+        }catch(InputException e){
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/inputExceptionPage.jsp");
+            requestDispatcher.forward(request, response);
+        }catch(NumberFormatException e){
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/numberFormatExceptionPage.jsp");
+            requestDispatcher.forward(request, response);
         }
     }
 
---
+++ src/main/webapp/numberFormatExceptionPage.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Exception</title>
+</head>
+<body>
+<h1><b>NumberFormatException</b></h1>
+</form>
+</body>
+</html>
